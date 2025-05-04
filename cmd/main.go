017a2ebf9d5cfe19b7a8482bbfbb80eeffbf515d
Rustam Khannanov <sk-dev@yandex.ru>
2025-05-04 13:27:13 +0300

    favicon, html template, css

--- cmd/main.go
+++ cmd/main.go
@@ -7,11 +7,15 @@ import (
 )
 
 const imageDir = "web/images"
+const cssDir = "web/css"
+const faviconDir = "web"
 const host = "localhost"
 const port = "8081"
 
 func main() {
 	http.Handle("/images/", http.StripPrefix("/images/", http.FileServer(http.Dir(imageDir))))
+	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir(cssDir))))
+	http.Handle("/favicon.ico", http.FileServer(http.Dir(faviconDir)))
 	http.HandleFunc("/", invitation.Handler)
 	fmt.Printf("Server listening on %s:%s", host, port)
 	http.ListenAndServe(fmt.Sprintf("%s:%s", host, port), nil)
--- internal/invitation/handler.go
+++ internal/invitation/handler.go
@@ -2,9 +2,14 @@ package invitation
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 	"strings"
 )
+
+type Data struct {
+	Url string
+}
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 	path := r.URL.Path
@@ -16,42 +21,24 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fullPath := fmt.Sprintf("web/images/%s.jpg", path)
+	fName := path + ".jpg"
+	fullPath := fmt.Sprintf("web/images/%s", fName)
 
 	if fileExists(fullPath) {
-		w.Header().Set("Content-Type", "text/html")
 
-		htmlContent := fmt.Sprintf(`
-		<!DOCTYPE html>
-		<html>
-		<head>
-		<title>Приглашение</title>
-		<meta charset="utf-8">
-		<style>
-		  body {
-			font-family: Arial, sans-serif;
-			background-color: #f0f0f0;
-			text-align: center;
-		  }
+		tmpl, err := template.ParseFiles("web/templates/invitation.html")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 
-		  h1 {
-			color: #333;
-		  }
+		data := Data{Url: "images/" + fName}
 
-		  p {
-			font-size: 1.2em;
-			color: #666;
-		  }
-		</style>
-		</head>
-		<body>
-		  <img src="images/%s.jpg"></img>
-		</body>
-		</html>
-		`, path)
-
-		fmt.Fprintf(w, htmlContent)
-
+		err = tmpl.Execute(w, data)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 		return
 	}
 
